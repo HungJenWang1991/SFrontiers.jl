50b7e06422897656de9ed21b77273390ab261379
Hung-Jen Wang <hjw.econ.1991@gmail.com>
2021-08-31 19:41:08 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -488,7 +488,7 @@ end
 	@test res.marginal[1,1] ≈ -0.08340  atol=1e-5
 	@test res.jlms[1] ≈ 1.21327  atol=1e-5
 	@test res.bc[1] ≈ 0.37075 atol=1e-5
-	@test boot1[1] ≈ 1.60067 atol=5e-5
+	@test boot1[1] ≈ 1.60067 atol=7e-5
 	@test d1[1] ≈ -0.47043 atol=1e-5
 
 	pred1 = sfmodel_predict(@eq(frontier))
